f03494848eea875e2cfff896c069bab275aaad73
nadudvari <nadudvari@github.com>
2017-11-29 14:44:00 +0100

    removed 'previous' variable

--- bowling.py
+++ bowling.py
@@ -3,20 +3,19 @@ def score(game):
     frame = 1
     in_first_half = True
     for i, v in enumerate(game):
-        last = get_value(game[i - 1])
         if game[i] == '/':
-            result += 10 - last
+            result += 10 - get_value(game[i - 1])
         else:
             result += get_value(game[i])
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
             elif game[i] == 'X' or game[i] == 'x':
-                result += get_value(game[i+1])
+                result += get_value(game[i + 1])
                 if game[i+2] == '/':
-                    result += 10 - get_value(game[i+1])
+                    result += 10 - get_value(game[i + 1])
                 else:
-                    result += get_value(game[i+2])
+                    result += get_value(game[i + 2])
         if not in_first_half:
             frame += 1
         if in_first_half is True:
